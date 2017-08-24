516f40ef5f9137098fba20700aadd933888d3508
Carlos Telis <carlos.telis@outlook.com>
2017-08-23 23:45:17 -0500

    Agrega listener a front-end

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(http);
 
 app.use(express.static(`${__dirname}/public`));
 
+// esto escucha cuando se carga la página
 io.on('connection', () => {
     console.log('User connected via socket.io');
 });
