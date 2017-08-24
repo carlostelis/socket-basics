a4e1cc4debea76ec9051b6d7cface1261d2e635c
Carlos Telis <carlos.telis@outlook.com>
2017-08-24 00:01:02 -0500

    Agrega eventos personalizados

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', () => {
     console.log('Conectado al servidor');
 });
+
+// Evento definido por programador
+socket.on('mensaje', (mensaje) => {
+    console.log(` > Nuevo mensaje: ${mensaje.text}`);
+});
--- server.js
+++ server.js
@@ -6,9 +6,23 @@ const io = require('socket.io')(http);
 
 app.use(express.static(`${__dirname}/public`));
 
-// esto escucha cuando se carga la página
-io.on('connection', () => {
+// No es para uso tradicional desde IP a IP
+// Escucha conexiones desde front-end to back-end
+// para transmisión de datos sin necesidad de refrescar
+io.on('connection', (socket) => {
     console.log('User connected via socket.io');
+
+    // Eventos definidos por programador
+    socket.on('mensaje',(mensaje) => {
+        console.log(`Mensaje recibido: ${mensaje.text}`);
+
+        // A todos los clientes
+        socket.broadcast.emit('mensaje', mensaje);
+    });
+
+    socket.emit('mensaje', {
+        text: 'Bienvenido al chat'
+    });
 });
 
 http.listen(PORT, () => {
